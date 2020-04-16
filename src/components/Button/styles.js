3da8cef7587d5354a315d5f9ecb52323c7eaaa51
Sandro Santos <sandro.dev@gmail.com>
2020-04-16 17:01:05 -0300

    component button update

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components/native';
-import { RectButton } from 'react-native-gesture-handler';
+import { BaseButton } from 'react-native-gesture-handler';
 
-export const Container = styled(RectButton)`
+import styled from 'styled-components/native';
+// import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(BaseButton)`
   height: 45px;
   background: #7d40e7;
-  padding: 12px 0;
+  padding: 12px 20px;
   border-radius: 4px;
   align-items: center;
   justify-content: center;
