506cca62cbc1561d2a7fbfa13c1997f97421d80a
Sandro Santos <sandro.dev@gmail.com>
2020-04-16 13:06:16 -0300

    routes to bottom tabs and stack navigate

---
+++ src/routes/Delivery.routes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Deliveries from '~/pages/Deliveries';
+import DeliveryDetail from '~/pages/DeliveryDetail';
+
+const Stack = createStackNavigator();
+
+export default function DeliveryRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTintColor: '#fff',
+        headerTransparent: true,
+      }}
+      initialRouteName="Entregas"
+    >
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Entregas"
+        component={Deliveries}
+      />
+      <Stack.Screen
+        name="Detalhes"
+        options={{
+          title: 'Detalhes da encomenda',
+        }}
+        component={DeliveryDetail}
+      />
+    </Stack.Navigator>
+  );
+}
