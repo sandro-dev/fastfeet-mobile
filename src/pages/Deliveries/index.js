72b4d2e22955ebcf30e1e9bf2bd47381e9f8dfb1
Sandro Santos <sandro.dev@gmail.com>
2020-04-17 01:26:20 -0300

    Deliveries Page

--- src/pages/Deliveries/index.js
+++ src/pages/Deliveries/index.js
@@ -1,8 +1,126 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { ActivityIndicator } from 'react-native';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import { signOut } from '~/store/modules/auth/actions';
+
+import Avatar from '~/components/Avatar';
+import CardItem from '~/components/CardItem';
+import formatDate from '~/util/formatDate';
+
+import {
+  Container,
+  Header,
+  WelcomeContainer,
+  WelcomeMessage,
+  WelcomeName,
+  BtnSignout,
+  Content,
+  PageTitle,
+  Filters,
+  BtnFilter,
+  BtnFilterText,
+  Feed,
+} from './styles';
 
 export default function Deliveries() {
-  return <View />;
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state?.user?.profile);
+  const auth = useSelector((state) => state.auth);
+
+  const [deliveries, setDeliveries] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalpages, setTotalpages] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    if (!auth.id) return;
+
+    async function loadDeliveries() {
+      const response = await api.get(
+        `deliverymen/${auth.id}/deliveries/${filter}`,
+        {
+          params: { page },
+        }
+      );
+
+      const data = response.data.results.map((delivery) => ({
+        ...delivery,
+        formated_start: formatDate(delivery.start_date),
+        formated_end: formatDate(delivery.end_date),
+      }));
+
+      setDeliveries(data);
+      setTotalpages(response.data.total_page);
+      setLoading(false);
+    }
+    loadDeliveries();
+  }, [page, totalpages, filter]);
+
+  async function handleLogout() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Header>
+        {profile?.avatar ? (
+          <Avatar isPhoto url={profile?.avatar?.url} />
+        ) : (
+          <Avatar fullname={profile?.name} style={{ size: 68, font: 40 }} />
+        )}
+
+        <WelcomeContainer>
+          <WelcomeMessage>Bem-vindo de volta,</WelcomeMessage>
+          <WelcomeName>{profile && profile.name}</WelcomeName>
+        </WelcomeContainer>
+
+        <BtnSignout onPress={handleLogout}>
+          <Icon name="exit-to-app" size={26} color="#E74040" />
+        </BtnSignout>
+      </Header>
+
+      <Content>
+        <PageTitle>Entregas</PageTitle>
+
+        <Filters>
+          <BtnFilter
+            onPress={() => {
+              setFilter('');
+              setIsActive(!isActive);
+            }}
+          >
+            <BtnFilterText isActive={isActive}>Pendentes</BtnFilterText>
+          </BtnFilter>
+          <BtnFilter
+            onPress={() => {
+              setFilter('finished');
+              setIsActive(!isActive);
+            }}
+          >
+            <BtnFilterText isActive={!isActive}>Entregues</BtnFilterText>
+          </BtnFilter>
+        </Filters>
+      </Content>
+
+      {loading ? (
+        <ActivityIndicator
+          color="#7d40e7"
+          size="large"
+          style={{ marginTop: 100 }}
+        />
+      ) : (
+        <Feed
+          data={deliveries}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <CardItem data={item} />}
+        />
+      )}
+    </Container>
+  );
 }
--- src/pages/Deliveries/styles.js
+++ src/pages/Deliveries/styles.js
@@ -1,3 +1,74 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { BaseButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+import Text from '~/components/Text';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+`;
+
+export const WelcomeContainer = styled.View`
+  margin: 20px 10px;
+`;
+
+export const WelcomeMessage = styled(Text)`
+  color: #666;
+`;
+
+export const WelcomeName = styled(Text)`
+  color: #444;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+export const BtnSignout = styled(BaseButton)`
+  background: #fff;
+  padding: 12px 20px;
+  border-radius: 4px;
+  margin-top: 10px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #444;
+`;
+
+export const Filters = styled.View`
+  flex-direction: row;
+`;
+
+export const BtnFilter = styled(BaseButton)`
+  font-size: 12px;
+  font-weight: bold;
+  margin-right: 10px;
+  color: ${(props) => (props.isActive ? '#7d40e7' : '#999')};
+  text-decoration: ${(props) => (props.isActive ? 'underline' : 'none')};
+`;
+
+export const BtnFilterText = styled.Text`
+  color: ${(props) => (props.isActive ? '#7d40e7' : '#999')};
+  font-weight: bold;
+`;
+
+export const Feed = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 20 },
+})``;
