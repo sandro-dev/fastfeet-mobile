ca6cdca91279dc8ea11deaead00db00b709d42cf
Sandro Santos <sandro.dev@gmail.com>
2020-04-16 23:56:01 -0300

    cardItem component

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation } from '@react-navigation/native';
+import CardStatus from '~/components/CardStatus';
+
+import {
+  Container,
+  TitleContainer,
+  Title,
+  Details,
+  Detail,
+  TitleDetail,
+  InfoDetail,
+  TextLink,
+} from './styles';
+
+export default function Delivey({ data }) {
+  const navigation = useNavigation();
+
+  return (
+    <Container>
+      <TitleContainer>
+        <Icon name="local-shipping" color="#7d40e7" size={20} />
+        <Title>Encomenda 0{data.id}</Title>
+      </TitleContainer>
+
+      <CardStatus status={data.status} />
+
+      <Details>
+        <Detail>
+          <TitleDetail>Data</TitleDetail>
+          <InfoDetail>{data.formated_start}</InfoDetail>
+        </Detail>
+
+        <Detail>
+          <TitleDetail>Cidade</TitleDetail>
+          <InfoDetail>{data.recipient.city}</InfoDetail>
+        </Detail>
+
+        <Detail>
+          <TextLink
+            onPress={() => navigation.navigate('Detalhes', { delivery: data })}
+          >
+            Ver detalhes
+          </TextLink>
+        </Detail>
+      </Details>
+    </Container>
+  );
+}
---
+++ src/components/CardItem/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+
+import Text from '~/components/Text';
+
+export const Container = styled.View`
+  border: 1px solid #eee;
+  border-radius: 4px;
+  padding-top: 15px;
+  margin-bottom: 30px;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 0 15px;
+`;
+
+export const Title = styled(Text)`
+  font-size: 14px;
+  font-weight: bold;
+  color: #7d40e7;
+  margin-left: 10px;
+`;
+
+export const Details = styled.View`
+  background: #f8f9fd;
+  margin-top: 20px;
+  padding: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Detail = styled.View`
+  justify-content: flex-end;
+`;
+
+export const TitleDetail = styled(Text)`
+  font-size: 10px;
+  color: #999;
+`;
+
+export const InfoDetail = styled(Text)`
+  font-size: 12px;
+  color: #444;
+  font-weight: bold;
+`;
+
+export const TextLink = styled(InfoDetail)`
+  color: #7d40e7;
+`;
