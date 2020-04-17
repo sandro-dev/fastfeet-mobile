43d00ce69c6623a84b6543b81855de6f7061aa1d
Sandro Santos <sandro.dev@gmail.com>
2020-04-16 23:18:46 -0300

    Profile Page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import Avatar from '~/components/Avatar';
+
+import {
+  Container,
+  Content,
+  ProfileDetails,
+  ProfileLabel,
+  ProfileInfo,
+  BtnLogout,
+} from './styles';
 
 export default function Profile() {
-  return <View />;
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state?.user?.profile);
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
+  console.tron.log('profile --> ', profile);
+
+  return (
+    <Container>
+      <Content>
+        {profile?.avatar ? (
+          // profile?.avatar?.url
+          <Avatar isPhoto url={profile?.avatar?.url} />
+        ) : (
+          <>{profile?.name && <Avatar fullname={profile?.name} />}</>
+        )}
+
+        <ProfileDetails>
+          <ProfileLabel>Nome Completo</ProfileLabel>
+          <ProfileInfo>{profile?.name}</ProfileInfo>
+          <ProfileLabel>Email</ProfileLabel>
+          <ProfileInfo>{profile?.email}</ProfileInfo>
+          <ProfileLabel>Data de cadastro</ProfileLabel>
+          <ProfileInfo>{profile?.created_at}</ProfileInfo>
+        </ProfileDetails>
+
+        <BtnLogout onPress={handleLogout} loading={false}>
+          Logout
+        </BtnLogout>
+      </Content>
+    </Container>
+  );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,38 @@
 import styled from 'styled-components/native';
+import Text from '~/components/Text';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background: #fff;
+  justify-content: center;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  padding: 0 36px;
+`;
+
+export const ProfileDetails = styled.View`
+  width: 100%;
+  padding-top: 40px;
+`;
+
+export const ProfileLabel = styled(Text)`
+  font-size: 12px;
+  color: #666;
+`;
+
+export const ProfileInfo = styled(Text)`
+  font-size: 22px;
+  font-weight: bold;
+  color: #444;
+  margin-bottom: 15px;
+`;
+
+export const BtnLogout = styled(Button)`
+  background: #e74040;
+  width: 100%;
+  height: 40px;
+  margin-top: 15px;
+`;
