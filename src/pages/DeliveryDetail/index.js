2fdbcc606d4f4bcf581d54e78955c4edb09275fb
Sandro Santos <sandro.dev@gmail.com>
2020-04-18 12:57:44 -0300

    DeliveryDetail page

--- src/pages/DeliveryDetail/index.js
+++ src/pages/DeliveryDetail/index.js
@@ -1,8 +1,130 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSelector } from 'react-redux';
+import { useRoute, useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import {
+  Container,
+  Background,
+  ContentContainer,
+  Card,
+  CardContainer,
+  CardColumns,
+  TitleContainer,
+  Title,
+  CardLabel,
+  CardInfo,
+  Status,
+  Actions,
+  Action,
+  ActionTitle,
+} from './styles';
 
 export default function DeliveryDetail() {
-  return <View />;
+  const auth = useSelector((state) => state.auth);
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { delivery } = route.params;
+
+  async function handleTakeDelivery() {
+    async function takeDelivery() {
+      try {
+        await api.put(`/deliverymen/${auth.id}/deliveries/${delivery.id}`, {
+          start_date: new Date(),
+        });
+      } catch (err) {
+        Alert.alert('Horário de retirda não permitido');
+      }
+    }
+
+    Alert.alert(
+      'Confirmar retirada',
+      'Deseja retirar esta encomenda para entrega?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'destructive',
+        },
+        {
+          text: 'Confirmar',
+          onPress: takeDelivery,
+        },
+      ],
+      {
+        cancelable: false,
+      }
+    );
+  }
+
+  return (
+    <Container>
+      <Background />
+      <ContentContainer>
+        <Card>
+          <TitleContainer>
+            <Icon name="local-shipping" color="#7d40e7" size={20} />
+            <Title>Informações da entrega</Title>
+          </TitleContainer>
+          <CardLabel>DESTINATÁRIO</CardLabel>
+          <CardInfo>{delivery.recipient.name}</CardInfo>
+          <CardLabel>ENDEREÇO DE ENTREGA</CardLabel>
+          <CardInfo>
+            {delivery.recipient.street}, {delivery.recipient.number},{' '}
+            {delivery.recipient.city} - {delivery.recipient.state},{' '}
+            {delivery.recipient.postcode}
+          </CardInfo>
+          <CardLabel>PRODUTO</CardLabel>
+          <CardInfo>{delivery.product}</CardInfo>
+        </Card>
+
+        <Card>
+          <TitleContainer>
+            <Icon name="event" color="#7d40e7" size={20} />
+            <Title>Situação da entrega</Title>
+          </TitleContainer>
+          <CardLabel>STATUS</CardLabel>
+          <Status>{delivery.status}</Status>
+          <CardColumns>
+            <CardContainer>
+              <CardLabel>DATA DE RETIRADA</CardLabel>
+              <CardInfo>{delivery.formated_start}</CardInfo>
+            </CardContainer>
+            <CardContainer>
+              <CardLabel>DATA DE ENTREGA</CardLabel>
+              <CardInfo>{delivery.formated_end}</CardInfo>
+            </CardContainer>
+          </CardColumns>
+        </Card>
+
+        <Actions>
+          <Action>
+            <Icon name="highlight-off" color="#E74040" size={20} />
+            <ActionTitle>Informar{`\n`}Problema</ActionTitle>
+          </Action>
+          <Action>
+            <Icon name="info-outline" color="#E7BA40" size={20} />
+            <ActionTitle>Visualizar{`\n`}Problemas</ActionTitle>
+          </Action>
+          {delivery.status === 'PENDENTE' ? (
+            <Action onPress={handleTakeDelivery}>
+              <Icon name="local-shipping" color="#7d40e7" size={20} />
+              <ActionTitle>Realizar{`\n`}Retirada</ActionTitle>
+            </Action>
+          ) : (
+            <Action
+              onPress={() =>
+                navigation.navigate('DeliveryConfirm', { delivery })
+              }
+            >
+              <Icon name="check-circle" color="#7d40e7" size={20} />
+              <ActionTitle>Confirmar{`\n`}Entrega</ActionTitle>
+            </Action>
+          )}
+        </Actions>
+      </ContentContainer>
+    </Container>
+  );
 }
--- src/pages/DeliveryDetail/styles.js
+++ src/pages/DeliveryDetail/styles.js
@@ -1,3 +1,85 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Text from '~/components/Text';
+import BgComponent from '~/components/Background';
+
+export const Container = styled.View`
+  background: #fff;
+  flex: 1;
+`;
+
+export const Background = styled(BgComponent)`
+  height: 155px;
+`;
+export const ContentContainer = styled.View`
+  margin-top: -90px;
+  margin-bottom: 20px;
+`;
+
+export const Card = styled.View`
+  background: #fff;
+  margin: 0 20px 10px 20px;
+  padding: 15px 30px 0 15px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+`;
+
+export const CardContainer = styled.View``;
+
+export const CardColumns = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled(Text)`
+  color: #7d40e7;
+  font-size: 14px;
+  font-weight: bold;
+  margin-left: 10px;
+`;
+
+export const CardLabel = styled(Text)`
+  color: #999;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+export const CardInfo = styled(Text)`
+  color: #666;
+  font-size: 14px;
+  margin-bottom: 15px;
+`;
+
+export const Status = styled(CardInfo)`
+  text-transform: capitalize;
+`;
+
+export const Actions = styled.View`
+  flex-direction: row;
+  background: #f8f9fd;
+  margin: 0 20px;
+  height: 83px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+`;
+
+export const Action = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #eee;
+`;
+
+export const ActionTitle = styled(Text)`
+  font-size: 12px;
+  color: #999;
+  margin-top: 5px;
+  text-align: center;
+`;
