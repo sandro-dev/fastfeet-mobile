64823bcdf487208bc00814d16727ef9440736585
Sandro Santos <sandro.dev@gmail.com>
2020-04-18 17:22:50 -0300

    Problems page

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import formatDate from '~/util/formatDate';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import {
+  Background,
+  Container,
+  TitleContainer,
+  ContentContainer,
+  ProblemFeed,
+  ProblemItem,
+  ProblemDescription,
+  ProblemDate,
+  Card,
+} from './styles';
 
 export default function Problems() {
-  return <View />;
+  const route = useRoute();
+  const { delivery } = route.params;
+  const [problems, setProblems] = useState([]);
+
+  useEffect(() => {
+    async function loadProblems() {
+      const response = await api.get(`delivery/${delivery.id}/problems`);
+      const data = response.data.results.map((problem) => ({
+        id: problem.id,
+        deliveryId: problem.delivery_id,
+        description: problem.description,
+        format_createdAt: formatDate(problem.createdAt),
+      }));
+      setProblems(data);
+    }
+
+    loadProblems();
+  }, [delivery.id]);
+
+  return (
+    <Container>
+      <Background />
+      <ContentContainer>
+        <TitleContainer>{`Encomenda ${delivery.id}`}</TitleContainer>
+        <ProblemFeed
+          data={problems}
+          keyExtractor={(problem) => String(problem.id)}
+          ListEmptyComponent={
+            <ProblemItem>
+              <ProblemDescription>
+                Nenhum problema encontrado
+              </ProblemDescription>
+            </ProblemItem>
+          }
+          renderItem={({ item }) => (
+            <ProblemItem>
+              <ProblemDescription>{item.description}</ProblemDescription>
+              <ProblemDate>{item.format_createdAt}</ProblemDate>
+            </ProblemItem>
+          )}
+        />
+      </ContentContainer>
+    </Container>
+  );
 }
--- src/pages/Problems/styles.js
+++ src/pages/Problems/styles.js
@@ -1,3 +1,52 @@
 import styled from 'styled-components/native';
+import Text from '~/components/Text';
+// import BgComponent from '~/components/Background';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background: #fff;
+  flex: 1;
+`;
+
+export const Background = styled.SafeAreaView`
+  height: 155px;
+  background: #7d40e7;
+`;
+
+export const ContentContainer = styled.View`
+  margin-top: -80px;
+  margin-bottom: 20px;
+`;
+
+export const TitleContainer = styled(Text)`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin: -10px auto 10px auto;
+`;
+
+export const ProblemFeed = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 20 },
+})``;
+
+export const ProblemItem = styled.View`
+  background: #fff;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  elevation: 1;
+  margin-bottom: 10px;
+  border-radius: 4px;
+  padding: 10px 20px;
+`;
+
+export const ProblemDescription = styled.Text`
+  color: #999;
+  font-size: 16px;
+  width: 200px;
+`;
+
+export const ProblemDate = styled.Text`
+  color: #c1c1c1;
+  font-size: 12px;
+`;
